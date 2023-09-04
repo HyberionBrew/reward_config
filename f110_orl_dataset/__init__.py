c7735ec208f686749164291a4d1e51d874da22f6
Fabian Kresse <fabian.kresse@mail.com>
2023-09-04 10:41:45 +0000

    updated to rl agents

--- f110_orl_dataset/__init__.py
+++ f110_orl_dataset/__init__.py
@@ -1,7 +1,7 @@
-from gym.envs.registration import register
+from gymnasium.envs.registration import register
 from .dataset_env import F1tenthDatasetEnv
 
-# This will override the existing registration
+# This will override the existing registration#
 register(
     id='f110_with_dataset-v0',
     entry_point='f110_orl_dataset.dataset_env:F1tenthDatasetEnv',
--- f110_orl_dataset/dataset_env.py
+++ f110_orl_dataset/dataset_env.py
@@ -1,80 +1,91 @@
-# import gymnasium as gym
-import gym
+import gymnasium as gym
 # import so PAth
 from pathlib import Path
 from f110_gym.envs.f110_env import F110Env
 # import spaces
-from gym.spaces import Box, Discrete
+from gymnasium.spaces import Box, Discrete
 import numpy as np
 
 import os 
 import sys
 import pickle
-import tensorflow as tf
 import zarr
 from typing import Union, Tuple, Dict, Optional, List, Any
 from f110_gym.envs import F110Env
-
 
 class F1tenthDatasetEnv(F110Env):
     def __init__(
         self,
         name,
         # f110_gym_kwarg,
+        root_dir=None,
         dataset_url=None,
         flatten_obs=True,
-        flatten_acts=True,
-        set_terminals=True,
         flatten_trajectories = False,
         laser_obs = True,
         subsample_laser = 10, # 1 is no subsampling
         padd_trajectories = True,
-        trajectory_max_length = None,
-        max_trajectories = None,
+        trajectory_max_length = 500,
+        as_trajectories = True,
         **kwargs
         ):
-        # Call the parent class's init
         print(kwargs)
         super(F1tenthDatasetEnv, self).__init__(**kwargs)
 
 
-        if not(set_terminals):
-            # not implemented yet
-            raise NotImplementedError("TODO! Implement non-terminal trajectories")
-        self.set_terminals = set_terminals
+        if flatten_trajectories:
+            raise NotImplementedError("TODO! Implement flatten_trajectories")
         self.name = name
+        self.root_dir = root_dir
         self.dataset_url = dataset_url
-        self.max_trajectories = max_trajectories
         self.padd_trajectories = padd_trajectories
         self.trajectory_max_length = trajectory_max_length
-        # self.sim_env = F110Env(**f110_gym_kwarg)
+        
         self.flatten_obs = flatten_obs
         self.subsample_laser = subsample_laser
+        self.as_trajectories = as_trajectories
         self.laser_obs = laser_obs
-        self.flatten_trajectories = flatten_trajectories
-        self.flatten_acts = flatten_acts
         # open with zarr
         # self.dataset = zarr.open(self.root_dir, mode='r')
         # list the subdirs
         # print(self.dataset.tree())
-
+        super(F1tenthDatasetEnv, self).__init__(**kwargs)
 
         # now we define our action and observation spaces
-        action_space_low = np.array([-np.pi/2,0])
-        action_space_high = np.array([np.pi/2, 10.0])
+        action_space_low = np.array([-1.0,-1])
+        action_space_high = np.array([1.0, 1.0])
         self.action_space = gym.spaces.Box(action_space_low, action_space_high)
         
         # observation space is a dict with keys:
         # the first one is the scan
         # the second one is the odometry
         rays = int(1080/subsample_laser)
-        laser_scan_space = gym.spaces.Box(low=np.array([0]*rays), high=np.array([30]*rays))
-        state_space = gym.spaces.Box(low=np.array([-100, -100, 0,-10.0, -10.0, -10.0]), high=np.array([100, 100,2*np.pi, 10,10,10]))
+        # laser_scan_space = gym.spaces.Box(low=np.array([0]*rays), high=np.array([30]*rays))
+        # Dict('ang_vels_z': Box(-20.0, 20.0, (1,), float32), 
+        # 'linear_vels_x': Box(-20.0, 20.0, (1,), float32), 
+        # 'linear_vels_y': Box(-20.0, 20.0, (1,), float32), 
+        # 'poses_theta': Box(-1e+30, 1e+30, (1,), float32), 
+        # 'poses_x': Box(-30.0, 30.0, (1,), float32), 
+        # 'poses_y': Box(-30.0, 30.0, (1,), float32), 
+        # 'progress': Box(0.0, 1.0, (1,), float32), 
+        # 'lidar_occupancy': Box(0, 255, (1, 80, 80), uint8), 
+        # 'previous_action': Box(-1.0, 1.0, (1, 2), float32))
+        #state_space = gym.spaces.Box(low=np.array([-100, -100, 0,-10.0, -10.0, -10.0]), high=np.array([100, 100,2*np.pi, 10,10,10]))
+        state_space = gym.spaces.Dict({'ang_vels_z': Box(-1.0, 1.0, (1,), np.float32),
+        'linear_vels_x': Box(-1.0, 1.0, (1,), np.float32),
+        'linear_vels_y': Box(-1.0, 1.0, (1,), np.float32),
+        'poses_theta': Box(-10, 10, (1,), np.float32), #TODO!
+        'poses_x': Box(-1.0, 1.0, (1,), np.float32),
+        'poses_y': Box(-1.0, 1.0, (1,), np.float32),
+        'progress': Box(0.0, 1.0, (1,), np.float32),
+        # 'lidar_occupancy': ,
+        'previous_action': Box(-1.0, 1.0, (1, 2), np.float32)})
         # print(state_space)
         self.observation_space = gym.spaces.Dict({
-            'laser_scan': laser_scan_space,
+            # 'laser_scan': Box(0, 255, (1, 80, 80), np.uint8),
             'state': state_space
         })
+        self.laser_obs_space = gym.spaces.Box(0, 255, (1, 80, 80), np.uint8)
         # print(self.observation_space)
         self._orig_flat_obs_space = gym.spaces.flatten_space(self.observation_space)
         
@@ -99,14 +110,32 @@ class F1tenthDatasetEnv(F110Env):
         # padd to max_length
         # for trajectories in 
         print([len(tra) for tra in trajectories])
+        print("hi")
         return dataset
+    
+    def get_change_indices(self, model_names):
+        change_indices = []
+        current_name = model_names[0]
+        for idx, name in enumerate(model_names):
+            if name != current_name:
+                change_indices.append(idx)
+                current_name = name
+        return change_indices + [len(model_names)]
+
+    def get_model_names(self, dataset):
+        pass
+    def get_scan(self, timesteps):
+        indices = timesteps - 1
+        root = zarr.open(self.zarr_path, mode='r')
+        scan = root['observations']['lidar_occupancy'][indices]
+        return scan
 
     def get_dataset(
         self,
         zarr_path: Union[str, os.PathLike] = None,
         clip: bool = True,
-        rng: Optional[Tuple[int, int]] = None,
-        indices: Optional[np.ndarray] = None,
+        # rng: Optional[Tuple[int, int]] = None,
+        without_agents: Optional[np.ndarray] = [],
     ) -> Dict[str, Any]:
         """ 
         TODO! this is copied from https://github.com/rr-learning/trifinger_rl_datasets/blob/master/trifinger_rl_datasets/dataset_env.py
@@ -149,119 +178,61 @@ class F1tenthDatasetEnv(F110Env):
         """
         # The ORL Dataset is loaded from Zarr arrays that look as follows:
         # TODO!
-        if rng or indices:
-            raise NotImplementedError("TODO! Implement rng and indices")
-        if rng is not None and indices is not None:
-            raise ValueError("rng and indices cannot be specified at the same time.")
-        
         if zarr_path is None:
             raise NotImplementedError("TODO! Download the dataset from the web")
             # zarr_path = self._download_dataset()
         
         #store = zarr.LMDBStore(zarr_path, readonly=True)
         print(zarr_path)
+        self.zarr_path = zarr_path
         root = zarr.open(zarr_path, mode='r') #(store=store)
 
         #print(root.tree())
-        trajectories = []
-        # find max key in root.group_keys()
-        # in order to have ordered trajectories
-        max_traj = max([int(i) for i in root.group_keys()]) 
-        if self.max_trajectories is None:
-            pass
-        else:
-            max_traj = min(max_traj, self.max_trajectories)
-
-        for trajectory in range(max_traj+1):
-            # print(trajectory)
-            trajectory = str(trajectory)
-            data_dict = {}
-            data_dict['rewards'] = np.array(root[trajectory]['rewards'][:])
-
-            data_dict['terminals'] = np.array(root[trajectory]['terminals'][:])
-            # array like terminals always false
-            if self.set_terminals:
-                data_dict['timeouts'] = np.zeros_like(data_dict['terminals'])
+        model_names = root['model_name'][:]
+        # print all unique model names
+        print(np.unique(model_names))
+        data_dict = {}
+        indices = np.where(~np.isin(model_names, without_agents))[0]
+        # print("Indices:", indices)
+        data_dict['rewards'] = root['rewards'][indices]
+        # print("hi")
+        data_dict['terminals'] = np.logical_or(root['done'][indices],root['truncated'][indices])
+        # print("hi")
+        data_dict['actions'] = root['actions'][indices]
+        # print("hi")
+        data_dict['timestep'] = root['timestep'][indices]
+        # loop over observation keys
+        data_dict['observations'] = dict()
+        for key in root['observations'].array_keys():
+            # print(key)
+            if key != 'lidar_occupancy':
+                data_dict['observations'][key] = root['observations'][key][indices]
+            else:
+                continue
+                # data_dict['scan'] = root['observations'][key][indices]
+        # data_dict = root['observations'][indices]
+        # New code for flattening
+        if self.flatten_obs:
+            # Extract all the filtered arrays and flatten them
+            # Expand dimensions and prepare arrays for concatenation
+            # print([arr.shape for arr in data_dict['observations'][key]])
+            arrays_to_concat = [data_dict['observations'][key].reshape([data_dict['observations'][key].shape[0], -1]) for key in data_dict['observations']]
+    
+            # print the shapes of all arrays to concatenate
+            # print([arr.shape for arr in arrays_to_concat])
+            # Concatenate all arrays along the last dimension
+            concatenated_obs = np.concatenate(arrays_to_concat, axis=-1)
             
-            data_dict['progress'] = np.array(root[trajectory]['progress'][:])
-            data_dict['infos'] = dict()
-            data_dict['infos']['agent'] = np.array(root[trajectory]['infos']['agent'][:])
-            data_dict['infos']['target_speed'] = np.array(root[trajectory]['infos']['target_speed'][:])
-            data_dict['infos']['index'] = np.array([trajectory] * len(data_dict['rewards']))
-            if self.flatten_acts:
-                data_dict['actions'] = np.vstack((root[trajectory]['actions']['steering'][:], root[trajectory]['actions']['velocity'][:]))
-            else:
-                data_dict['actions']['steering'] = np.array(root[trajectory]['actions']['steering'][:])
-                data_dict['actions']['velocity'] = np.array(root[trajectory]['actions']['velocity'][:])
-                #root[trajectory]['infos']
-            if self.flatten_obs:
-                # just concatenate the state values (each in an array in observations)
-                # exepct the laser scan
-                observation_keys = root[trajectory]['observations'].array_keys()
-                # numpy array of size rewards
-                obs = np.empty((0,len(data_dict['rewards'])))
-                data_dict['obs_keys'] = []
-                for key in observation_keys:
-                    if key != 'scan':
-                        data_dict['obs_keys'].append(key)
-                        #print(obs.shape)
-                        #print(key)
-                        #print(root[trajectory]['observations'][key][:].shape)
-                        obs = np.vstack((obs, root[trajectory]['observations'][key][:]))
-                data_dict['obs_keys'] = np.array(data_dict['obs_keys'])
-                data_dict['observations'] = obs
-            else:
-                raise NotImplementedError("TODO! Implement non-flattened observations")
-                        
-            if self.laser_obs:
-                data_dict['scan'] = root[trajectory]['observations']['scan'][:,::self.subsample_laser]
-
-            trajectories.append(data_dict)
+            # Reshape the concatenated array: [timesteps, -1] means keeping the number of timesteps the same, but flattening all other dimensions.
+            data_dict['observations'] = concatenated_obs.reshape([concatenated_obs.shape[0], -1])
+            
+        data_dict['infos'] = dict()
+        # print("hi")
+        data_dict['infos']['model_name']= root['model_name'][indices]
+        return data_dict
         
-        if self.flatten_trajectories:
-            assert(self.flatten_obs)
-            traj_dict = dict()
-            # loop over all trajectories
-            for i, trajectory in enumerate(trajectories):
-                # loop over all keys in trajectory
-                for key in trajectory.keys():
-                    # if the key is not in traj_dict, add it
-                    if key not in traj_dict.keys():
-                        assert(i==0)
-                        if isinstance(trajectory[key], dict):
-                            traj_dict[key] = dict()
-                            for sub_key in trajectory[key].keys():
-                                traj_dict[key][sub_key] = np.array([])
-                        else:
-                            # like the array assoicated with the key
-                            if isinstance(trajectory[key], np.ndarray):
-                                if trajectory[key].ndim == 1:
-                                    traj_dict[key] = np.array([])
-                                else:
-                                    traj_dict[key] = np.empty((trajectory[key].shape[0],0))
-                            else:
-                                traj_dict[key] = np.array([])
-                    # concatenate the key
-
-                    # if trajectory[key] is a dict unroll
-                    if isinstance(trajectory[key], dict):
-                        for sub_key in trajectory[key].keys():
-                            #print(f"{key}{sub_key}")
-                            #print(traj_dict[key][sub_key].shape)
-                            #print(trajectory[key][sub_key])
-                            traj_dict[key][sub_key] = np.concatenate((traj_dict[key][sub_key], trajectory[key][sub_key])) 
-                    else:
-                        #print(f"{key}")
-                        # concatenate along last dimension
-                        #print(traj_dict[key].shape)
-                        #print(trajectory[key].shape)
-                        traj_dict[key] = np.concatenate((traj_dict[key], trajectory[key]), axis=-1)
-            # print(traj_dict.keys())
-            print(traj_dict['observations'].shape)
-            traj_dict['observations'] = traj_dict['observations'].T
-            traj_dict['actions'] = traj_dict['actions'].T
-            print(traj_dict['observations'].shape)
-            print("hi")
-            trajectories = traj_dict
-            print(trajectories.keys())
-        return trajectories
+    def observation_spec(self):
+        return self.observation_space
+    
+    def action_spec(self):
+        return self.action_space
